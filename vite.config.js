c51480fe89da62e2d05aba1766c2ba9cdbdf7ab0
prudvikrishnamarisetty-sys <prudvikrishnamarisetty@gmail.com>
2026-05-19 20:26:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: "/smart-exam-preparation-project/",
+  plugins: [react()],
+})
+
 
 export default defineConfig({
   plugins: [react()],
